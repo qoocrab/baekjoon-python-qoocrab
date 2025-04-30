cbb51248ae2eb0c22166500ef988f2630d2a4a8d
Hayeon Kim <qoocrab@gmail.com>
2025-05-01 07:03:04 +0900

    ♻️ refactor: simplify I/O method for problem 02741
    - Removed unnecessary sys module usage
    - Replaced sys.stdin.readline()/sys.stdout.write() with built-in input()/print()

--- bronze_05/solution_02741.py
+++ bronze_05/solution_02741.py
@@ -1,11 +1,6 @@
 # Time Complexity: O(N)
 # Space Complexity: O(1)
-import sys
-
-input = sys.stdin.readline
-write = sys.stdout.write
-
 n = int(input())
 
 for i in range(1, n + 1):
-    write(f"{i}\n")
+    print(i)
